2e249e629d9e28ac6d6df04c3032670b2f4d53ff
Oskru <oskrupaa@gmail.com>
2023-05-26 21:07:02 +0200

    Refractor jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@
 import DishForm from './components/DishForm/DishForm';
 
 function App() {
-  console.log('Render: App');
-
   return (
     <>
-      <h1>HexOcean</h1>
-      <h2>Dishes task implementation</h2>
+      <h1>Add a dish!</h1>
       <DishForm />
     </>
   );
