963eb625dea95e0104a8c2c31d792804a3a825f2
Oskru <oskrupaa@gmail.com>
2023-05-26 18:35:08 +0200

    Fix select input validation

--- src/schemas/dish_schema.js
+++ src/schemas/dish_schema.js
@@ -4,25 +4,26 @@ export const dishSchema = Yup.object().shape({
   name: Yup.string().required('name of dish is required'),
   preparation_time: Yup.string().required('preparation_time is required'),
   type: Yup.string()
-    .required('type is required')
-    .oneOf(['pizza', 'soup', 'sandwich']),
+    .oneOf(['pizza', 'soup', 'sandwich'], 'invalid type')
+    .required('type is required'),
   no_of_slices: Yup.number().when('type', {
     is: 'pizza',
-    then: Yup.number().required('no_of_slices is required'),
+    then: () => Yup.number().required('no_of_slices is required'),
   }),
   diameter: Yup.number().when('type', {
     is: 'pizza',
-    then: Yup.number().required('diameter is required'),
+    then: () => Yup.number().required('diameter is required'),
   }),
   spiciness_scale: Yup.number().when('type', {
     is: 'soup',
-    then: Yup.number()
-      .min(1, 'spiciness_scale must be between 1 and 10')
-      .max(10, 'spiciness_scale must be between 1 and 10')
-      .required('spiciness_scale is required'),
+    then: () =>
+      Yup.number()
+        .min(1, 'spiciness_scale must be between 1 and 10')
+        .max(10, 'spiciness_scale must be between 1 and 10')
+        .required('spiciness_scale is required'),
   }),
   slices_of_bread: Yup.number().when('type', {
     is: 'sandwich',
-    then: Yup.number().required('slices_of_bread is required'),
+    then: () => Yup.number().required('slices_of_bread is required'),
   }),
 });
