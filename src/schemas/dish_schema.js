2059e754d35c60b86e4f78e7ae2b44fb35538cbe
Oskru <oskrupaa@gmail.com>
2023-05-26 21:07:46 +0200

    Add proper preparation_time validation

--- src/schemas/dish_schema.js
+++ src/schemas/dish_schema.js
@@ -1,8 +1,17 @@
 import * as Yup from 'yup';
 
+const preparation_time_regex =
+  /^(?:[0-9]{1,2}|[0-8][0-9]):[0-5][0-9]:[0-5][0-9]$/;
+// Matches HH:MM:SS format, while HH is between 00-99
+
 export const dishSchema = Yup.object().shape({
   name: Yup.string().required('name of dish is required'),
-  preparation_time: Yup.string().required('preparation_time is required'),
+  preparation_time: Yup.string()
+    .matches(
+      preparation_time_regex,
+      'preparation_time has to be in valid hh:mm:ss format',
+    )
+    .required('preparation_time is required'),
   type: Yup.string()
     .oneOf(['pizza', 'soup', 'sandwich'], 'invalid type')
     .required('type is required'),
