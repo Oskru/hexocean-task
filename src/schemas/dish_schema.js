321ab73c2bbca5ce9c63b5e3fc72c90c01ea8dc0
Oskru <oskrupaa@gmail.com>
2023-05-29 01:46:06 +0200

    Improve Yup from validation coverage

--- src/schemas/dish_schema.js
+++ src/schemas/dish_schema.js
@@ -1,15 +1,16 @@
 import * as Yup from 'yup';
 
-const preparation_time_regex =
-  /^(?:[0-9]{1,2}|[0-8][0-9]):[0-5][0-9]:[0-5][0-9]$/;
-// Matches HH:MM:SS format, while HH is between 00-99
+const preparation_time_regex = /^(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d$/;
+// matches HH:MM:SS format
 
 export const dishSchema = Yup.object().shape({
-  name: Yup.string().required('name of dish is required'),
+  name: Yup.string()
+    .min(3, 'name must be at least 3 characters')
+    .required('name of dish is required'),
   preparation_time: Yup.string()
     .matches(
       preparation_time_regex,
-      'preparation_time has to be in valid hh:mm:ss format',
+      'preparation_time has to be in valid hh:mm:ss format (max 23:59:59)',
     )
     .required('preparation_time is required'),
   type: Yup.string()
@@ -17,11 +18,17 @@ export const dishSchema = Yup.object().shape({
     .required('type is required'),
   no_of_slices: Yup.number().when('type', {
     is: 'pizza',
-    then: () => Yup.number().required('no_of_slices is required'),
+    then: () =>
+      Yup.number()
+        .min(1, 'no_of_slices must be min. 1')
+        .required('no_of_slices is required'),
   }),
   diameter: Yup.number().when('type', {
     is: 'pizza',
-    then: () => Yup.number().required('diameter is required'),
+    then: () =>
+      Yup.number()
+        .min(1, 'diameter must be min. 1 cm')
+        .required('diameter is required'),
   }),
   spiciness_scale: Yup.number().when('type', {
     is: 'soup',
@@ -33,6 +40,9 @@ export const dishSchema = Yup.object().shape({
   }),
   slices_of_bread: Yup.number().when('type', {
     is: 'sandwich',
-    then: () => Yup.number().required('slices_of_bread is required'),
+    then: () =>
+      Yup.number()
+        .min(1, 'slices_of_bread must be min. 1')
+        .required('slices_of_bread is required'),
   }),
 });
